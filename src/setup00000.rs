c70b3062c476d0f84a5848ba4005b97c6e9bf43b
Mark Mansi <markm@cs.wisc.edu>
2018-11-21 21:03:02 -0600

    only build kernel if branch is passed

--- src/setup00000.rs
+++ src/setup00000.rs
@@ -10,8 +10,6 @@ use spurs::{cmd, ssh::SshShell};
 const VAGRANT_RPM_URL: &str =
     "https://releases.hashicorp.com/vagrant/2.1.5/vagrant_2.1.5_x86_64.rpm";
 
-const DEFAULT_GIT_BRANCH: &str = "markm_ztier";
-
 pub fn run<A: std::net::ToSocketAddrs + std::fmt::Display>(
     dry_run: bool,
     cloudlab: A,
@@ -74,99 +72,96 @@ pub fn run<A: std::net::ToSocketAddrs + std::fmt::Display>(
     }
 
     // clone linux-dev
-    ushell.run(cmd!("mkdir -p linux-dev"))?;
-    ushell.run(cmd!("git init").cwd(&format!("/users/{}/linux-dev", username)))?;
-    ushell.run(
-        cmd!("git checkout -b side")
-            .cwd(&format!("/users/{}/linux-dev", username))
-            .allow_error(), // if already exists
-    )?;
+    if let Some(git_branch) = git_branch {
+        ushell.run(cmd!("mkdir -p linux-dev"))?;
+        ushell.run(cmd!("git init").cwd(&format!("/users/{}/linux-dev", username)))?;
+        ushell.run(
+            cmd!("git checkout -b side")
+                .cwd(&format!("/users/{}/linux-dev", username))
+                .allow_error(), // if already exists
+        )?;
 
-    let git_branch = if let Some(git_branch) = git_branch {
-        git_branch
-    } else {
-        DEFAULT_GIT_BRANCH
-    };
+        if !dry_run {
+            let _ = Command::new("git")
+                .args(&["checkout", git_branch])
+                .current_dir("/u/m/a/markm/private/large_mem/software/linux-dev")
+                .status()?;
 
-    if !dry_run {
-        let _ = Command::new("git")
-            .args(&["checkout", git_branch])
-            .current_dir("/u/m/a/markm/private/large_mem/software/linux-dev")
-            .status()?;
+            let _ = Command::new("git")
+                .args(&[
+                    "push",
+                    "-u",
+                    &format!("ssh://{}/users/{}/linux-dev", cloudlab, username),
+                    git_branch,
+                ])
+                .current_dir("/u/m/a/markm/private/large_mem/software/linux-dev")
+                .status()?;
+        }
+        ushell.run(
+            cmd!("git checkout {}", git_branch).cwd(&format!("/users/{}/linux-dev", username)),
+        )?;
 
-        let _ = Command::new("git")
-            .args(&[
-                "push",
-                "-u",
-                &format!("ssh://{}/users/{}/linux-dev", cloudlab, username),
-                git_branch,
-            ])
-            .current_dir("/u/m/a/markm/private/large_mem/software/linux-dev")
-            .status()?;
-    }
-    ushell
-        .run(cmd!("git checkout {}", git_branch).cwd(&format!("/users/{}/linux-dev", username)))?;
-
-    // compile linux-dev
-    ushell.run(cmd!("mkdir -p /users/{}/linux-dev/kbuild", username))?;
-    ushell.run(
-        cmd!("make O=/users/{}/linux-dev/kbuild defconfig", username)
-            .cwd(&format!("/users/{}/linux-dev", username)),
-    )?;
-    let config = ushell
-        .run(cmd!("ls -1 /boot/config-* | head -n1").use_bash())?
-        .stdout;
-    let config = config.trim();
-    ushell.run(cmd!(
-        "cp {} /users/{}/linux-dev/kbuild/.config",
-        config,
-        username
-    ))?;
-    ushell.run(
-        cmd!("yes '' | make oldconfig")
-            .use_bash()
-            .cwd(&format!("/users/{}/linux-dev/kbuild", username)),
-    )?;
-
-    // make sure some configurations are set/not set
-    const CONFIG_SET: &[&str] = &[
-        "CONFIG_ZSWAP",
-        "CONFIG_ZPOOL",
-        "CONFIG_ZBUD",
-        "CONFIG_ZTIER",
-        "CONFIG_SBALLOC",
-        "CONFIG_ZSMALLOC",
-    ];
-    for opt in CONFIG_SET.iter() {
+        // compile linux-dev
+        ushell.run(cmd!("mkdir -p /users/{}/linux-dev/kbuild", username))?;
+        ushell.run(
+            cmd!("make O=/users/{}/linux-dev/kbuild defconfig", username)
+                .cwd(&format!("/users/{}/linux-dev", username)),
+        )?;
+        let config = ushell
+            .run(cmd!("ls -1 /boot/config-* | head -n1").use_bash())?
+            .stdout;
+        let config = config.trim();
         ushell.run(cmd!(
-            "sed -i 's/# {} is not set/{}=y/' /users/{}/linux-dev/kbuild/.config",
-            opt,
-            opt,
+            "cp {} /users/{}/linux-dev/kbuild/.config",
+            config,
             username
         ))?;
+        ushell.run(
+            cmd!("yes '' | make oldconfig")
+                .use_bash()
+                .cwd(&format!("/users/{}/linux-dev/kbuild", username)),
+        )?;
+
+        // make sure some configurations are set/not set
+        const CONFIG_SET: &[&str] = &[
+            "CONFIG_ZSWAP",
+            "CONFIG_ZPOOL",
+            "CONFIG_ZBUD",
+            "CONFIG_ZTIER",
+            "CONFIG_SBALLOC",
+            "CONFIG_ZSMALLOC",
+        ];
+        for opt in CONFIG_SET.iter() {
+            ushell.run(cmd!(
+                "sed -i 's/# {} is not set/{}=y/' /users/{}/linux-dev/kbuild/.config",
+                opt,
+                opt,
+                username
+            ))?;
+        }
+
+        let nprocess = ushell.run(cmd!("getconf _NPROCESSORS_ONLN"))?.stdout;
+        let nprocess = nprocess.trim();
+        ushell.run(
+            cmd!("make -j {} binrpm-pkg LOCALVERSION=-ztier", nprocess)
+                .cwd(&format!("/users/{}/linux-dev/kbuild", username))
+                .allow_error(),
+        )?;
+        ushell.run(
+            cmd!("make -j {} binrpm-pkg LOCALVERSION=-ztier", nprocess)
+                .cwd(&format!("/users/{}/linux-dev/kbuild", username)),
+        )?;
+
+        // install linux-dev
+        ushell.run(
+            cmd!("sudo yum -y install `ls -t1 | head -n2 | sort`")
+                .use_bash()
+                .cwd(&format!("/users/{}/rpmbuild/RPMS/x86_64/", username)),
+        )?;
+
+        // update grub to choose this entry (new kernel) by default
+        ushell.run(cmd!("sudo grub2-set-default 0"))?;
     }
-
-    let nprocess = ushell.run(cmd!("getconf _NPROCESSORS_ONLN"))?.stdout;
-    let nprocess = nprocess.trim();
-    ushell.run(
-        cmd!("make -j {} binrpm-pkg LOCALVERSION=-ztier", nprocess)
-            .cwd(&format!("/users/{}/linux-dev/kbuild", username))
-            .allow_error(),
-    )?;
-    ushell.run(
-        cmd!("make -j {} binrpm-pkg LOCALVERSION=-ztier", nprocess)
-            .cwd(&format!("/users/{}/linux-dev/kbuild", username)),
-    )?;
-
-    // install linux-dev
-    ushell.run(
-        cmd!("sudo yum -y install `ls -t1 | head -n2 | sort`")
-            .use_bash()
-            .cwd(&format!("/users/{}/rpmbuild/RPMS/x86_64/", username)),
-    )?;
-
-    // update grub to choose this entry (new kernel) by default
-    ushell.run(cmd!("sudo grub2-set-default 0"))?;
 
     // change image location
     ushell.run(cmd!("mkdir -p vm_shared/results/"))?;
