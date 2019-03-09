cf5ce106291d1a679ec2447521c22cbf1347a261
Mark Mansi <markm@cs.wisc.edu>
2019-03-09 14:56:45 -0600

    actually fix minor bug with setup 0

--- runner/src/setup00000.rs
+++ runner/src/setup00000.rs
@@ -276,7 +276,7 @@ where
     vrshell.run(cmd!("grub2-mkconfig -o /boot/grub2/grub.cfg"))?;
 
     // Need to run shutdown to make sure that the next host reboot doesn't lose guest data.
-    vrshell.run(cmd!("sudo poweroff").allow_error())?;
+    let _ = vrshell.run(cmd!("sudo poweroff")); // This will give a TCP error for obvious reasons
 
     Ok(())
 }
