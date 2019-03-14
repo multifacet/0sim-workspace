dcdcab9febef9119a2c452c6bca0ffb84881f186
Mark Mansi <markm@cs.wisc.edu>
2019-03-13 19:48:49 -0500

    documentation

--- runner/src/common/output.rs
+++ runner/src/common/output.rs
@@ -1,9 +1,17 @@
-//! Utilities for handling generated output.
-
-// TODO: documentation
+//! Utilities for handling and tagging generated output.
 
 use serde::{ser::SerializeMap, Deserialize, Deserializer, Serialize, Serializer};
 
+/// `OutputManager` manages all things regarding naming and tagging output with settings and
+/// properties of its data.
+///
+/// Each experiment should create an `OutputManager` at the beginning with all of the settings for
+/// the experiment. The `settings!` macro helper can be used to do this conveniently. The
+/// `OutputManager` can then be used to generate filenames for output files and can generate a
+/// `.params` file containing all of the settings.
+///
+/// The generated filenames will be unique by including a timestamp. They can also optionally
+/// contain any settings marked as `important`.
 #[derive(Debug, Clone)]
 pub struct OutputManager {
     settings: std::collections::BTreeMap<String, String>,
@@ -11,6 +19,7 @@ pub struct OutputManager {
 }
 
 impl OutputManager {
+    /// Create a new empty `OutputManager` containing now settings.
     pub fn new() -> Self {
         OutputManager {
             settings: std::collections::BTreeMap::new(),
@@ -18,6 +27,9 @@ impl OutputManager {
         }
     }
 
+    /// Register a new setting called `name` with value `value`. The boolean value `important`
+    /// indicates whether or not the setting should be included in any generated filenames. All
+    /// settings must be serializable.
     pub fn register<V: serde::Serialize + std::fmt::Debug>(
         &mut self,
         name: &str,
@@ -36,10 +48,21 @@ impl OutputManager {
         }
     }
 
+    /// Generate the primary output and params filenames, in that order.
     pub fn gen_file_names(&self) -> (String, String) {
         const OUTPUT_SUFFIX: &str = "out";
         const PARAMS_SUFFIX: &str = "params";
 
+        (
+            self.gen_file_name(OUTPUT_SUFFIX),
+            self.gen_file_name(PARAMS_SUFFIX),
+        )
+    }
+
+    /// Generate a filename with the given extension. Only use this if you want to generate a file
+    /// that is not a `.out` or a `.params` file. The parameter `ext` is the extension without the
+    /// leading dot (e.g. `err`).
+    pub fn gen_file_name(&self, ext: &str) -> String {
         let mut base = String::new();
 
         // prepend all important settings
@@ -59,11 +82,13 @@ impl OutputManager {
         );
 
         base.push_str(".");
+        base.push_str(ext);
 
-        let base_clone = base.clone();
-        (base + OUTPUT_SUFFIX, base_clone + PARAMS_SUFFIX)
+        base
     }
 
+    /// Helper to add the given setting to the given string. Used to build file names. The caller
+    /// should ensure that the setting is registered.
     fn append_setting(&self, string: &mut String, setting: &str) {
         let val = self
             .settings
@@ -80,6 +105,12 @@ impl OutputManager {
         string.push_str(&val);
     }
 
+    /// Returns the value of setting `setting` deserialized to a `D`.
+    ///
+    /// # Panics
+    ///
+    /// - If `setting` is not registered at the time `get` is called.
+    /// - If `setting`'s value cannot be deserialized to a `D`.
     pub fn get<'s, 'de, D: serde::Deserialize<'de>>(&'s self, setting: &str) -> D
     where
         's: 'de,
@@ -131,6 +162,23 @@ macro_rules! __settings_helper {
     }};
 }
 
+/// A convenience macro for creating an `OutputManager` with the given settings. The syntax is `[*]
+/// name: value` where `name` is the name of the setting, `value` is any expression that evaluates
+/// to the value of the setting, and the * is an optional token that signifies that the setting is
+/// important.
+///
+/// ```rust
+/// let settings: OutputManager = settings! {
+///     * workload: if pattern.is_some() { "time_mmap_touch" } else { "memcached_gen_data" },
+///     exp: 00000,
+///
+///     * size: 100, // gb
+///     pattern: "-z",
+///     calibrated: false,
+///     warmup: true,
+/// };
+///
+/// ```
 #[macro_export]
 macro_rules! settings {
     ($($tail:tt)*) => {{
