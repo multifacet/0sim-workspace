88770b6ca3bf4ccda9966d378aea2af42720caf7
Mark Mansi <markm@cs.wisc.edu>
2019-08-08 20:10:56 -0500

    oops

--- runner/src/setup00000.rs
+++ runner/src/setup00000.rs
@@ -1047,8 +1047,8 @@ where
     }
 
     // Create a mountpoint for nullfs
-    vushell.run(cmd!("sudo chmod 777 /mnt/nullfs"))?;
     vushell.run(cmd!("sudo mkdir -p /mnt/nullfs"))?;
+    vushell.run(cmd!("sudo chmod 777 /mnt/nullfs"))?;
 
     Ok(())
 }
