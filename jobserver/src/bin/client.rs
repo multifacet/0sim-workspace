9354877c6fa6b5e7055282d798fee012367d6b83
Mark Mansi <markm@cs.wisc.edu>
2019-09-19 11:43:24 -0500

    start on matrix csv dump

--- jobserver/src/bin/client.rs
+++ jobserver/src/bin/client.rs
@@ -136,6 +136,15 @@ fn main() {
                     (@arg LONG: --long
                      "Show all output")
                 )
+
+                (@subcommand csv =>
+                    (about: "Output information about the matrix and current/finished jobs \
+                     to a CSV file.")
+                    (@arg ID: +required {is_usize}
+                     "The matrix ID of the matrix")
+                    (@arg FILE: +required
+                     "The name of the file to output to. The file is truncated if it exists.")
+                )
             )
         )
     }
@@ -330,6 +339,31 @@ fn handle_matrix_cmd(addr: &str, matches: &clap::ArgMatches<'_>) {
                 JobServerResp::MatrixStatus { mut jobs, .. } => {
                     let jobs = stat_jobs(addr, &mut jobs);
                     print_jobs(jobs, is_long);
+                }
+                _ => println!("Server response: {:?}", response),
+            }
+        }
+
+        ("csv", Some(sub_m)) => {
+            let response = make_request(
+                addr,
+                JobServerReq::StatMatrix {
+                    id: sub_m.value_of("ID").unwrap().parse().unwrap(),
+                },
+            );
+
+            let file = sub_m.value_of("FILE").unwrap();
+
+            match response {
+                JobServerResp::MatrixStatus {
+                    mut jobs,
+                    cp_results: _cp_results,
+                    id,
+                    variables,
+                    ..
+                } => {
+                    let jobs = stat_jobs(addr, &mut jobs);
+                    make_matrix_csv(file, id, variables, jobs);
                 }
                 _ => println!("Server response: {:?}", response),
             }
@@ -641,6 +675,77 @@ fn print_avail(machines: Vec<MachineInfo>) {
     }
 
     table.printstd();
+}
+
+fn make_matrix_csv(
+    file: &str,
+    id: usize,
+    variables: HashMap<String, Vec<String>>,
+    jobs: Vec<JobInfo>,
+) {
+    match make_matrix_csv_inner(file, id, variables, jobs) {
+        Err(e) => panic!("Failed to create CSV: {}", e),
+        Ok(()) => {}
+    }
+}
+
+fn make_matrix_csv_inner(
+    file: &str,
+    id: usize,
+    variables: HashMap<String, Vec<String>>,
+    jobs: Vec<JobInfo>,
+) -> Result<(), failure::Error> {
+    let mut csvw = csv::WriterBuilder::new()
+        .has_headers(true)
+        .from_path(file)?;
+
+    // The format of the CSV we want:
+    //  matrix id, class, cmd (with replacements), jid, status or results file, var1, var2, var3, ... , varN
+
+    // Collect the variables (in some deterministic order)
+    let vars: Vec<_> = variables.keys().map(String::as_str).collect();
+
+    // Write the headers for the CSV
+    let mut headers = vec!["Matrix ID", "Class", "Command", "Job ID", "Status"];
+    headers.extend(vars.iter());
+    csvw.write_record(headers)?;
+
+    // Write the content of the CSV
+    for job in jobs.into_iter() {
+        let mut record = vec![
+            id.to_string(),
+            job.class,
+            job.cmd,
+            job.jid.to_string(),
+            match job.status {
+                Status::Cancelled => "Cancelled".to_owned(),
+                Status::Waiting => "Waiting".to_owned(),
+                Status::Running { .. } => "Running".to_owned(),
+                Status::Failed { .. } => "Failed".to_owned(),
+
+                Status::Done { output: None, .. } => "Done (no output)".to_owned(),
+
+                Status::Done {
+                    output: Some(path), ..
+                } => path,
+            },
+        ];
+
+        for var in vars.iter() {
+            record.push(
+                job.variables
+                    .get(*var)
+                    .cloned()
+                    .unwrap_or_else(|| "<missing>".to_owned()),
+            );
+        }
+
+        csvw.write_record(record)?;
+    }
+
+    csvw.flush()?;
+
+    Ok(())
 }
 
 fn is_usize(s: String) -> Result<(), String> {
