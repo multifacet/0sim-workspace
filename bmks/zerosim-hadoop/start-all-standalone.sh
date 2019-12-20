7573068a46b8fb71a91744f6c50a93a263050c5b
Mark Mansi <markm@cs.wisc.edu>
2019-12-20 17:49:30 -0600

    need to start mr job history server

--- bmks/zerosim-hadoop/start-all-standalone.sh
+++ bmks/zerosim-hadoop/start-all-standalone.sh
@@ -4,6 +4,9 @@ jps
 
 $HADOOP_HOME/sbin/start-dfs.sh
 $HADOOP_HOME/sbin/start-yarn.sh
+
+$HADOOP_HOME/bin/mapred --daemon start historyserver
+
 #$SPARK_HOME/sbin/start-all.sh
 
 jps
--- bmks/zerosim-hadoop/stop-all-standalone.sh
+++ bmks/zerosim-hadoop/stop-all-standalone.sh
@@ -2,6 +2,8 @@
 
 jps
 
+$HADOOP_HOME/bin/mapred --daemon stop historyserver
+
 $SPARK_HOME/sbin/stop-all.sh
 $HADOOP_HOME/sbin/stop-yarn.sh
 $HADOOP_HOME/sbin/stop-dfs.sh
