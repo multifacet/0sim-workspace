306f1668e8be91ecfa96e9234a8b6049ac3f548c
Mark Mansi <markm@cs.wisc.edu>
2019-12-31 11:17:49 -0600

    start spark too

--- bmks/zerosim-hadoop/start-all-standalone.sh
+++ bmks/zerosim-hadoop/start-all-standalone.sh
@@ -7,6 +7,7 @@ $HADOOP_HOME/sbin/start-yarn.sh
 
 $HADOOP_HOME/bin/mapred --daemon start historyserver
 
-#$SPARK_HOME/sbin/start-all.sh
+$SPARK_HOME/sbin/start-master.sh -h localhost -p 7077
+$SPARK_HOME/sbin/start-slave.sh localhost:707
 
 jps
